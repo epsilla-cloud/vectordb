8181a57a33ce585d53a0ce6389620963e86a7c12
rickiEpsilla <ricki@epsilla.com>
2023-07-16 22:09:46 -0700

    catalog endpoints;

--- engine/server/web_server/handler/web_request_handler.hpp
+++ engine/server/web_server/handler/web_request_handler.hpp
@@ -1,120 +1,120 @@
-// #pragma once
+#pragma once
 
-// #include <map>
-// #include <memory>
-// #include <string>
-// #include <unordered_map>
-// #include <utility>
-// #include <vector>
+#include <map>
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
 
-// #include <oatpp/core/data/mapping/type/Object.hpp>
-// #include <oatpp/core/macro/codegen.hpp>
-// #include <oatpp/web/server/api/ApiController.hpp>
+#include <oatpp/core/data/mapping/type/Object.hpp>
+#include <oatpp/core/macro/codegen.hpp>
+#include <oatpp/web/server/api/ApiController.hpp>
 
-// #include "utils/json.hpp"
-// #include "utils/status.hpp"
+#include "utils/json.hpp"
+#include "utils/status.hpp"
 
-// namespace vectordb {
-// namespace server {
-// namespace web {
+namespace vectordb {
+namespace server {
+namespace web {
 
-// #define RETURN_STATUS_DTO(STATUS_CODE, MESSAGE)      \
-//     do {                                             \
-//         auto status_dto = StatusDto::createShared(); \
-//         status_dto->code = (STATUS_CODE);            \
-//         status_dto->message = (MESSAGE);             \
-//         return status_dto;                           \
-//     } while (false);
+#define RETURN_STATUS_DTO(STATUS_CODE, MESSAGE)      \
+    do {                                             \
+        auto status_dto = StatusDto::createShared(); \
+        status_dto->code = (STATUS_CODE);            \
+        status_dto->message = (MESSAGE);             \
+        return status_dto;                           \
+    } while (false);
 
-// #define ASSIGN_RETURN_STATUS_DTO(STATUS)                    \
-//     do {                                                    \
-//         int code;                                           \
-//         if (0 != (STATUS).code()) {                         \
-//             code = WebErrorMap((STATUS).code());            \
-//         } else {                                            \
-//             code = 0;                                       \
-//         }                                                   \
-//         RETURN_STATUS_DTO(code, (STATUS).message().c_str()) \
-//     } while (false);
+#define ASSIGN_RETURN_STATUS_DTO(STATUS)                    \
+    do {                                                    \
+        int code;                                           \
+        if (0 != (STATUS).code()) {                         \
+            code = WebErrorMap((STATUS).code());            \
+        } else {                                            \
+            code = 0;                                       \
+        }                                                   \
+        RETURN_STATUS_DTO(code, (STATUS).message().c_str()) \
+    } while (false);
 
-// StatusCode
-// WebErrorMap(ErrorCode code);
+StatusCode
+WebErrorMap(ErrorCode code);
 
-// class WebRequestHandler {
-//  private:
-//     std::shared_ptr<Context>
-//     GenContextPtr(const std::string& context_str) {
-//         auto context_ptr = std::make_shared<Context>("dummy_request_id");
-//         context_ptr->SetTraceContext(trace_context);
+class WebRequestHandler {
+ private:
+    // std::shared_ptr<Context>
+    // GenContextPtr(const std::string& context_str) {
+    //     auto context_ptr = std::make_shared<Context>("dummy_request_id");
+    //     context_ptr->SetTraceContext(trace_context);
 
-//         return context_ptr;
-//     }
+    //     return context_ptr;
+    // }
 
-//  private:
-//     void
-//     AddStatusToJson(vectordb::Json& json, int64_t code, const std::string& msg);
+ private:
+    // void
+    // AddStatusToJson(vectordb::Json& json, int64_t code, const std::string& msg);
 
-//     Status
-//     IsBinaryCollection(const std::string& collection_name, bool& bin);
+    // Status
+    // IsBinaryCollection(const std::string& collection_name, bool& bin);
 
-//     // Status
-//     // CopyRecordsFromJson(const vectordb::Json& json, engine::VectorsData& vectors, bool bin);
+    // Status
+    // CopyRecordsFromJson(const vectordb::Json& json, engine::VectorsData& vectors, bool bin);
 
-//  protected:
-//     Status
-//     CommandLine(const std::string& cmd, std::string& reply);
+ protected:
+    // Status
+    // CommandLine(const std::string& cmd, std::string& reply);
 
-//     Status
-//     Cmd(const std::string& cmd, std::string& result_str);
+    // Status
+    // Cmd(const std::string& cmd, std::string& result_str);
 
-//     Status
-//     PreLoadCollection(const vectordb::Json& json, std::string& result_str);
+    // Status
+    // PreLoadCollection(const vectordb::Json& json, std::string& result_str);
 
-//     Status
-//     ReleaseCollection(const vectordb::Json& json, std::string& result_str);
+    // Status
+    // ReleaseCollection(const vectordb::Json& json, std::string& result_str);
 
-//     Status
-//     Flush(const vectordb::Json& json, std::string& result_str);
+    // Status
+    // Flush(const vectordb::Json& json, std::string& result_str);
 
-//     Status
-//     Compact(const vectordb::Json& json, std::string& result_str);
+    // Status
+    // Compact(const vectordb::Json& json, std::string& result_str);
 
-//     Status
-//     GetConfig(std::string& result_str);
+    // Status
+    // GetConfig(std::string& result_str);
 
-//     Status
-//     SetConfig(const vectordb::Json& json, std::string& result_str);
+    // Status
+    // SetConfig(const vectordb::Json& json, std::string& result_str);
 
-//     Status
-//     Search(const std::string& collection_name, const vectordb::Json& json, std::string& result_str);
+    // Status
+    // Search(const std::string& collection_name, const vectordb::Json& json, std::string& result_str);
 
-//     Status
-//     DeleteByIDs(const std::string& collection_name, const vectordb::Json& json, std::string& result_str);
+    // Status
+    // DeleteByIDs(const std::string& collection_name, const vectordb::Json& json, std::string& result_str);
 
-//     Status
-//     GetVectorsByIDs(const std::string& collection_name, const std::string& partition_tag,
-//                     const std::vector<int64_t>& ids, vectordb::Json& json_out);
+    // Status
+    // GetVectorsByIDs(const std::string& collection_name, const std::string& partition_tag,
+    //                 const std::vector<int64_t>& ids, vectordb::Json& json_out);
 
-//  public:
-//     WebRequestHandler() {
-//         context_ptr_ = GenContextPtr("Web Handler");
-//         request_handler_ = RequestHandler();
-//     }
+ public:
+    // WebRequestHandler() {
+    //     context_ptr_ = GenContextPtr("Web Handler");
+    //     request_handler_ = RequestHandler();
+    // }
 
-//  public:
+ public:
 
-//  public:
-//     void
-//     RegisterRequestHandler(const RequestHandler& handler) {
-//         request_handler_ = handler;
-//     }
+ public:
+    // void
+    // RegisterRequestHandler(const RequestHandler& handler) {
+    //     request_handler_ = handler;
+    // }
 
-//  private:
-//     std::shared_ptr<Context> context_ptr_;
-//     RequestHandler request_handler_;
-//     std::unordered_map<std::string, engine::meta::hybrid::DataType> field_type_;
-// };
+ private:
+    // std::shared_ptr<Context> context_ptr_;
+    // RequestHandler request_handler_;
+    // std::unordered_map<std::string, engine::meta::hybrid::DataType> field_type_;
+};
 
-// }
-// }
-// }
+}
+}
+}
--- engine/server/web_server/web_controller.hpp
+++ engine/server/web_server/web_controller.hpp
@@ -8,8 +8,13 @@
 #include <oatpp/parser/json/mapping/ObjectMapper.hpp>
 #include <oatpp/web/server/api/ApiController.hpp>
 
+#include "utils/json.hpp"
+#include "utils/status.hpp"
+#include "server/web_server/dto/db_dto.hpp"
 #include "server/web_server/dto/status_dto.hpp"
-// #include "server/web_server/handler/web_request_handler.hpp"
+#include "server/web_server/handler/web_request_handler.hpp"
+#include "db/catalog/meta.hpp"
+#include "db/catalog/basic_meta_impl.hpp"
 
 #define WEB_LOG_PREFIX "[Web] "
 
@@ -49,6 +54,45 @@ class WebController : public oatpp::web::server::api::ApiController {
         dto->statusCode = 200;
         dto->message = "Server is online!";
         return createDtoResponse(Status::CODE_200, dto);
+    }
+
+    ADD_CORS(LoadDB)
+
+    ENDPOINT("POST", "/api/load", LoadDB, BODY_STRING(String, body)) {
+        vectordb::Json parsedBody;
+        parsedBody.LoadFromString(body);
+        std::string dbPath = parsedBody.GetString("path");
+        std::string dbName = parsedBody.GetString("name");
+        vectordb::engine::meta::MetaPtr meta = std::make_shared<vectordb::engine::meta::BasicMetaImpl>();
+        vectordb::Status status = meta->LoadDatabase(dbPath, dbName);
+        if (!status.ok()) {
+            return createResponse(Status::CODE_501, "Load/Create " + dbName + " failed.");
+        }
+        return createResponse(Status::CODE_200, "Load/Create " + dbName + " successfully.");
+    }
+
+    ADD_CORS(UnloadDB)
+
+    ENDPOINT("POST", "api/{db_name}/unload", UnloadDB, PATH(String, dbName, "db_name")) {
+        vectordb::engine::meta::MetaPtr meta = std::make_shared<vectordb::engine::meta::BasicMetaImpl>();
+        vectordb::Status status = meta->UnloadDatabase(dbName);
+        if (!status.ok()) {
+            return createResponse(Status::CODE_501, "Unload " + dbName + " failed.");
+        }
+
+        return createResponse(Status::CODE_200, "Unload " + dbName + " successfully.");
+    }
+
+    ADD_CORS(DropDB)
+
+    ENDPOINT("DELETE", "api/{db_name}/drop", DropDB, PATH(String, dbName, "db_name")) {
+        vectordb::engine::meta::MetaPtr meta = std::make_shared<vectordb::engine::meta::BasicMetaImpl>();
+        vectordb::Status status = meta->DropDatabase(dbName);
+        if (!status.ok()) {
+            return createResponse(Status::CODE_501, "Drop " + dbName + " failed.");
+        }
+
+        return createResponse(Status::CODE_200, "Drop " + dbName + " successfully.");
     }
 
 /**
