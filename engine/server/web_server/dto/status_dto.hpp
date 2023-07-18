8bf3e8d5b028fb5aa8e9fb9ab669738bed41b1ec
rickiEpsilla <ricki@epsilla.com>
2023-07-18 00:38:07 -0700

    create table;

--- engine/server/web_server/dto/status_dto.hpp
+++ engine/server/web_server/dto/status_dto.hpp
@@ -3,6 +3,8 @@
 #include <oatpp/core/Types.hpp>
 #include <oatpp/core/macro/codegen.hpp>
 
+// #include "db/catalog/meta.hpp"
+
 namespace vectordb {
 namespace server {
 namespace web {
@@ -16,6 +18,14 @@ class StatusDto: public oatpp::DTO {
     DTO_FIELD(Int32, statusCode);
     DTO_FIELD(String, message);
 };
+
+// class SchemaInfoDto: public oatpp::DTO {
+//     DTO_INIT(SchemaInfoDto, DTO)
+
+//     DTO_FIELD(Int32, statusCode);
+//     DTO_FIELD(String, message);
+//     DTO_FIELD(Object<vectordb::engine::meta::TableSchema>, schema);
+// };
 
 #include OATPP_CODEGEN_END(DTO)
 
