a7fdba030fcb0001e61aee97024137bccb777942
TopKeyboard <lululemonmonitor@gmail.com>
2023-08-06 14:23:50 -0700

    add more methods

--- engine/bindings/python/interface.cpp
+++ engine/bindings/python/interface.cpp
@@ -1,5 +1,6 @@
 #define PY_SSIZE_T_CLEAN
 #include <string>
+#include <vector>
 #include <Python.h>
 #include "bindings/python/interface.h"
 #include "db/db_server.hpp"
@@ -7,7 +8,8 @@
 
 const std::string tableSchemaKey_name = "name",
                   tableSchemaKey_dataType = "dataType",
-                  tableSchemaKey_dimensions = "dimensions";
+                  tableSchemaKey_dimensions = "dimensions",
+                  tableSchemaKey_autoEmbedding = "autoEmbedding";
 
 PyMODINIT_FUNC
 PyInit_epsilla(void)
@@ -33,34 +35,51 @@ PyInit_epsilla(void)
   return m;
 }
 
-static PyObject *load_db(PyObject *self, PyObject *args)
+static PyObject *load_db(PyObject *self, PyObject *args, PyObject *kwargs)
 {
+  static const char *keywords[] = {"db_name", "db_path", NULL};
   const char *namePtr, *pathPtr;
-  int sts;
 
-  if (!PyArg_ParseTuple(args, "ss", &namePtr, &pathPtr))
+  if (!PyArg_ParseTupleAndKeywords(args, kwargs, "ss", (char **)keywords, &namePtr, &pathPtr))
     return NULL;
+
   auto name = std::string(namePtr), path = std::string(pathPtr);
+  if (name.empty())
+  {
+    PyErr_SetString(PyExc_Exception, "empty db name");
+    return NULL;
+  }
+
+  if (path.empty())
+  {
+    PyErr_SetString(PyExc_Exception, "empty path name");
+    return NULL;
+  }
+
   auto status = db->LoadDB(name, path, vectordb::server::web::InitTableScale);
   return PyLong_FromLong(status.code());
 }
 
-static PyObject *use_db(PyObject *self, PyObject *args)
+static PyObject *use_db(PyObject *self, PyObject *args, PyObject *kwargs)
 {
+  static const char *keywords[] = {"db_name", NULL};
   const char *namePtr;
 
-  if (!PyArg_ParseTuple(args, "s", &namePtr))
+  if (!PyArg_ParseTupleAndKeywords(args, kwargs, "s", (char **)keywords, &namePtr))
     return NULL;
 
   db_name = namePtr;
   return PyLong_FromLong(0);
 }
-static PyObject *create_table(PyObject *self, PyObject *args)
+
+static PyObject *create_table(PyObject *self, PyObject *args, PyObject *kwargs)
 {
+
+  static const char *keywords[] = {"table_name", "table_fields", NULL};
   const char *tableNamePtr;
   PyObject *tableFieldsListPtr;
 
-  if (!PyArg_ParseTuple(args, "sO", &tableNamePtr, &tableFieldsListPtr))
+  if (!PyArg_ParseTupleAndKeywords(args, kwargs, "sO", (char **)keywords, &tableNamePtr, &tableFieldsListPtr))
     return NULL;
 
   vectordb::engine::meta::TableSchema schema;
@@ -139,9 +158,12 @@ static PyObject *create_table(PyObject *self, PyObject *args)
       Py_DECREF(dimensionsValue);
       schema.fields_.push_back(field);
     }
-
     Py_DECREF(dict_obj);
   }
+
+  Py_DECREF(tableFieldsListPtr);
+
+  // TODO: add auto embedding here
 
   auto status = db->CreateTable(db_name, schema);
   if (!status.ok())
@@ -151,5 +173,167 @@ static PyObject *create_table(PyObject *self, PyObject *args)
   }
   return PyLong_FromLong(int(status.code()));
 }
-static PyObject *insert(PyObject *self, PyObject *args) {}
-static PyObject *query(PyObject *self, PyObject *args) {}
+static PyObject *insert(PyObject *self, PyObject *args, PyObject *kwargs)
+{
+
+  static const char *keywords[] = {"table_name", "records", NULL};
+  const char *tableNamePtr;
+  PyObject *recordListPtr;
+
+  if (!PyArg_ParseTupleAndKeywords(args, kwargs, "sO", (char **)keywords, &tableNamePtr, &recordListPtr))
+    return NULL;
+
+  std::string tableName = tableNamePtr;
+
+  PyObject *json_module = PyImport_ImportModule("json");
+
+  if (json_module == NULL)
+  {
+    PyErr_SetString(PyExc_Exception, "Unable to import json module");
+    return NULL;
+  }
+
+  PyObject *dumps_func = PyObject_GetAttrString(json_module, "dumps");
+  Py_DECREF(json_module);
+
+  if (dumps_func == NULL || !PyCallable_Check(dumps_func))
+  {
+    PyErr_SetString(PyExc_Exception, "Unable to get address of json.dumps method");
+    Py_XDECREF(dumps_func);
+    return NULL;
+  }
+
+  PyObject *args_tuple = PyTuple_Pack(1, recordListPtr);
+  PyObject *kwargs_dict = PyDict_New(); // You can pass keyword arguments here if needed
+
+  PyObject *json_str = PyObject_Call(dumps_func, args_tuple, kwargs_dict);
+
+  Py_DECREF(dumps_func);
+  Py_DECREF(args_tuple);
+  Py_DECREF(kwargs_dict);
+
+  if (json_str == NULL)
+  {
+    PyErr_SetString(PyExc_Exception, "unable to dump records as JSON");
+    return NULL;
+  }
+
+  // Convert the PyObject to a C-style string (UTF-8)
+  const char *utf8_str = PyUnicode_AsUTF8(json_str);
+  Py_DECREF(json_str);
+
+  if (utf8_str == NULL)
+  {
+    return NULL;
+  }
+
+  auto records = vectordb::Json();
+  records.LoadFromString(std::string(utf8_str));
+  auto status = db->Insert(db_name, tableName, records);
+  return PyLong_FromLong(int(status.code()));
+}
+
+static PyObject *query(PyObject *self, PyObject *args, PyObject *kwargs)
+{
+  static const char *keywords[] = {"table_name", "query_field", "query_vector", "limit", NULL};
+  const char *tableNamePtr, *queryFieldPtr;
+  int limit;
+  PyObject *queryVector;
+
+  if (!PyArg_ParseTupleAndKeywords(args, kwargs, "ssOi", (char **)keywords, &tableNamePtr, &queryFieldPtr, &queryVector, &limit))
+  {
+    return NULL;
+  }
+  auto queryFields = std::vector<std::string>();
+
+  Py_ssize_t queryVectorSize = PyList_Size(queryVector);
+
+  std::string tableName = tableNamePtr, queryField = queryFieldPtr;
+  std::unique_ptr<float[]> vectorArr(new float[queryVectorSize]);
+  for (Py_ssize_t i = 0; i < queryVectorSize; ++i)
+  {
+    PyObject *elem = PyList_GetItem(queryVector, i);
+    vectorArr[i] = PyFloat_AsDouble(elem);
+    Py_DecRef(elem);
+  }
+  auto result = vectordb::Json();
+  auto status = db->Search(db_name, tableName, queryField, queryFields, queryVectorSize, vectorArr.get(), limit, result);
+  if (!status.ok())
+  {
+    PyErr_SetString(PyExc_Exception, status.message().c_str());
+    return NULL;
+  }
+
+  // Import the json module
+  PyObject *json_module = PyImport_ImportModule("json");
+  if (json_module == NULL)
+  {
+    PyErr_SetString(PyExc_Exception, "Unable to import json module");
+    return NULL;
+  }
+
+  // Get a reference to the json.loads function
+  PyObject *loads_func = PyObject_GetAttrString(json_module, "loads");
+  if (loads_func == NULL || !PyCallable_Check(loads_func))
+  {
+    PyErr_SetString(PyExc_Exception, "Unable to get address of json.loads method");
+    Py_XDECREF(loads_func);
+    return NULL;
+  }
+  PyObject *resultString = PyUnicode_FromString(result.DumpToString().c_str());
+  PyObject *args_tuple = PyTuple_Pack(1, resultString);
+  PyObject *kwargs_dict = PyDict_New(); // You can pass keyword arguments here if needed
+
+  PyObject *response = PyObject_Call(loads_func, args_tuple, kwargs_dict);
+
+  Py_DECREF(loads_func);
+  Py_DECREF(args_tuple);
+  Py_DECREF(kwargs_dict);
+
+  if (response == NULL)
+  {
+    PyErr_SetString(PyExc_Exception, "unable to json.loads response from string");
+    return NULL;
+  }
+
+  PyObject *ret = PyTuple_Pack(2, PyLong_FromLong(long(status.code())), response);
+  return ret;
+}
+
+static PyObject *drop_table(PyObject *self, PyObject *args, PyObject *kwargs)
+{
+  static const char *keywords[] = {"table_name", NULL};
+  const char *tableNamePtr;
+
+  if (!PyArg_ParseTupleAndKeywords(args, kwargs, "s", (char **)keywords, &tableNamePtr))
+    return NULL;
+
+  auto tableName = std::string(tableNamePtr);
+  if (tableName.empty())
+  {
+    PyErr_SetString(PyExc_Exception, "empty table name");
+    return NULL;
+  }
+
+  auto status = db->DropTable(db_name, tableName);
+  return PyLong_FromLong(status.code());
+}
+
+static PyObject *unload_db(PyObject *self, PyObject *args, PyObject *kwargs)
+{
+  static const char *keywords[] = {"db_name", NULL};
+  const char *dbNamePtr;
+
+  if (!PyArg_ParseTupleAndKeywords(args, kwargs, "s", (char **)keywords, &dbNamePtr))
+    return NULL;
+
+  auto dbName = std::string(dbNamePtr);
+  if (dbName.empty())
+  {
+    PyErr_SetString(PyExc_Exception, "empty db name");
+    return NULL;
+  }
+
+  auto status = db->DropTable(db_name, dbName);
+  return PyLong_FromLong(status.code());
+}
--- engine/bindings/python/interface.h
+++ engine/bindings/python/interface.h
@@ -5,21 +5,25 @@
 #include "db/db_server.hpp"
 static PyObject *EpsillaError;
 
-static PyObject *load_db(PyObject *self, PyObject *args);
-static PyObject *use_db(PyObject *self, PyObject *args);
-static PyObject *create_table(PyObject *self, PyObject *args);
-static PyObject *insert(PyObject *self, PyObject *args);
-static PyObject *query(PyObject *self, PyObject *args);
+static PyObject *load_db(PyObject *self, PyObject *args, PyObject *kwargs);
+static PyObject *use_db(PyObject *self, PyObject *args, PyObject *kwargs);
+static PyObject *create_table(PyObject *self, PyObject *args, PyObject *kwargs);
+static PyObject *insert(PyObject *self, PyObject *args, PyObject *kwargs);
+static PyObject *query(PyObject *self, PyObject *args, PyObject *kwargs);
+static PyObject *drop_table(PyObject *self, PyObject *args, PyObject *kwargs);
+static PyObject *unload_db(PyObject *self, PyObject *args, PyObject *kwargs);
 
 static std::string db_name;
 static vectordb::engine::DBServer *db;
 
 static PyMethodDef EpsillaMethods[] = {
-    {"load_db", load_db, METH_VARARGS, "Load the database"},
-    {"use_db", use_db, METH_VARARGS, "Use the database"},
-    {"create_table", create_table, METH_VARARGS, "Use create the table"},
-    {"insert", insert, METH_VARARGS, "insert record into the database"},
-    {"query", query, METH_VARARGS, "query the database"},
+    {"load_db", (PyCFunction)(void (*)(void))load_db, METH_VARARGS | METH_KEYWORDS, "Load the database"},
+    {"unload_db", (PyCFunction)(void (*)(void))unload_db, METH_VARARGS | METH_KEYWORDS, "Unload the database"},
+    {"use_db", (PyCFunction)(void (*)(void))use_db, METH_VARARGS | METH_KEYWORDS, "Use the database"},
+    {"create_table", (PyCFunction)(void (*)(void))create_table, METH_VARARGS | METH_KEYWORDS, "create a table"},
+    {"insert", (PyCFunction)(void (*)(void))insert, METH_VARARGS | METH_KEYWORDS, "insert record into the database"},
+    {"query", (PyCFunction)(void (*)(void))query, METH_VARARGS | METH_KEYWORDS, "query the database"},
+    {"drop_table", (PyCFunction)(void (*)(void))drop_table, METH_VARARGS | METH_KEYWORDS, "drop the table"},
     {NULL, NULL, 0, NULL} /* Sentinel */
 };
 
