2bc1c0fbf1bb5f2bc7e3df779c284460c999d03b
TopKeyboard <lululemonmonitor@gmail.com>
2023-11-24 13:43:36 -0800

    add unittest for sparse vector

--- engine/test/engine/db/sparse_vector.cpp
+++ engine/test/engine/db/sparse_vector.cpp
@@ -6,9 +6,10 @@
 #include <vector>
 
 using vectordb::engine::GetCosineDist;
+using vectordb::engine::GetL2Dist;
+using vectordb::engine::GetL2DistSqr;
 using vectordb::engine::SparseVector;
 
-// Demonstrate some basic assertions.
 TEST(SparseVector, GetCosineDist) {
   SparseVector v1 = {{0, 1}, {1, 0}};
   auto totalSteps = 360;
@@ -19,5 +20,35 @@ TEST(SparseVector, GetCosineDist) {
     auto y = std::sin(theta) * (step + 1);
     SparseVector v2 = {{0, x}, {1, y}};
     EXPECT_FLOAT_EQ(GetCosineDist(v1, v2), std::cos(theta));
+  }
+}
+
+TEST(SparseVector, GetL2Dist) {
+  SparseVector v1 = {{0, 1}, {1, 0}};
+  auto totalSteps = 360;
+  for (int step = 0; step < totalSteps; step++) {
+    float theta = 2 * M_PI / totalSteps * step;
+    // make the vector length more than 1
+    auto x = std::cos(theta) * (step + 1);
+    auto y = std::sin(theta) * (step + 1);
+    SparseVector v2 = {{0, x}, {1, y}};
+
+    auto dist = std::sqrt((x - 1) * (x - 1) + y * y);
+    EXPECT_FLOAT_EQ(GetL2Dist(v1, v2), dist);
+  }
+}
+
+TEST(SparseVector, GetL2DistSqr) {
+  SparseVector v1 = {{0, 1}, {1, 0}};
+  auto totalSteps = 360;
+  for (int step = 0; step < totalSteps; step++) {
+    float theta = 2 * M_PI / totalSteps * step;
+    // make the vector length more than 1
+    auto x = std::cos(theta) * (step + 1);
+    auto y = std::sin(theta) * (step + 1);
+    SparseVector v2 = {{0, x}, {1, y}};
+
+    auto dist = (x - 1) * (x - 1) + y * y;
+    EXPECT_FLOAT_EQ(GetL2DistSqr(v1, v2), dist);
   }
 }
