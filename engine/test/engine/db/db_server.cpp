2fb3ad7f781ff50ef6bc12ddcd65b0e7e01fc898
TopKeyboard <lululemonmonitor@gmail.com>
2023-11-28 12:03:26 -0800

    add unittest for rebuild

--- engine/test/engine/db/db_server.cpp
+++ engine/test/engine/db/db_server.cpp
@@ -3,11 +3,13 @@
 
 #include <gtest/gtest.h>
 
+#include <chrono>  // std::chrono::seconds
 #include <cstdio>
 #include <cstdlib>
 #include <filesystem>
 #include <memory>
 #include <string>
+#include <thread>  // std::this_thread::sleep_for
 #include <vector>
 
 TEST(DbServer, CreateTable) {
@@ -738,183 +740,30 @@ TEST(DbServer, RebuildDenseVector) {
   std::string tempDir = std::filesystem::temp_directory_path() / std::filesystem::path("ut_db_server_dense_vector");
   vectordb::engine::DBServer database;
   std::filesystem::remove_all(tempDir);
-  const auto dbName = "MyDb";
-  const auto tableName = "MyTable";
-  size_t queryDimension = 4;
-  database.LoadDB(dbName, tempDir, 150000, true);
   size_t tableId = 0;
+  auto dbName = "MyDB";
+  vectordb::Json schemaObj;
 
-  const std::string schema = R"_(
-{
-  "name": "MyTable",
-  "fields": [
-    {
-      "name": "ID",
-      "dataType": "INT",
-      "primaryKey": true
-    },
-    {
-      "name": "Doc",
-      "dataType": "STRING"
-    },
-    {
-      "name": "EmbeddingEuclidean",
-      "dataType": "VECTOR_FLOAT",
-      "dimensions": 4,
-      "metricType": "EUCLIDEAN"
-    },
-    {
-      "name": "EmbeddingDotProduct",
-      "dataType": "VECTOR_FLOAT",
-      "dimensions": 4,
-      "metricType": "DOT_PRODUCT"
-    },
-    {
-      "name": "EmbeddingCosine",
-      "dataType": "VECTOR_FLOAT",
-      "dimensions": 4,
-      "metricType": "COSINE"
-    }
-  ]
-}
-    )_";
+  // read schema
+  std::ifstream schemaStream("engine/db/testdata/dense_schema_1.json");
+  std::string schema;
+  schemaStream.seekg(0, std::ios::end);
+  schema.reserve(schemaStream.tellg());
+  schemaStream.seekg(0, std::ios::beg);
+  schema.assign((std::istreambuf_iterator<char>(schemaStream)),
+                std::istreambuf_iterator<char>());
+  schemaObj.LoadFromString(schema);
+  auto tableName = schemaObj.GetString("name");
 
-  const std::string records = R"_(
-[
-  {
-    "ID": 1,
-    "Doc": "Berlin",
-    "EmbeddingEuclidean": [
-      0.05,
-      0.61,
-      0.76,
-      0.74
-    ],
-    "EmbeddingDotProduct": [
-      0.05,
-      0.61,
-      0.76,
-      0.74
-    ],
-    "EmbeddingCosine": [
-      0.05,
-      0.61,
-      0.76,
-      0.74
-    ]
-  },
-  {
-    "ID": 2,
-    "Doc": "London",
-    "EmbeddingEuclidean": [
-      0.19,
-      0.81,
-      0.75,
-      0.11
-    ],
-    "EmbeddingDotProduct": [
-      0.19,
-      0.81,
-      0.75,
-      0.11
-    ],
-    "EmbeddingCosine": [
-      0.19,
-      0.81,
-      0.75,
-      0.11
-    ]
-  },
-  {
-    "ID": 3,
-    "Doc": "Moscow",
-    "EmbeddingEuclidean": [
-      0.36,
-      0.55,
-      0.47,
-      0.94
-    ],
-    "EmbeddingDotProduct": [
-      0.36,
-      0.55,
-      0.47,
-      0.94
-    ],
-    "EmbeddingCosine": [
-      0.36,
-      0.55,
-      0.47,
-      0.94
-    ]
-  },
-  {
-    "ID": 4,
-    "Doc": "San Francisco",
-    "EmbeddingEuclidean": [
-      0.18,
-      0.01,
-      0.85,
-      0.8
-    ],
-    "EmbeddingDotProduct": [
-      0.18,
-      0.01,
-      0.85,
-      0.8
-    ],
-    "EmbeddingCosine": [
-      0.18,
-      0.01,
-      0.85,
-      0.8
-    ]
-  },
-  {
-    "ID": 5,
-    "Doc": "Shanghai",
-    "EmbeddingEuclidean": [
-      0.24,
-      0.18,
-      0.22,
-      0.44
-    ],
-    "EmbeddingDotProduct": [
-      0.24,
-      0.18,
-      0.22,
-      0.44
-    ],
-    "EmbeddingCosine": [
-      0.24,
-      0.18,
-      0.22,
-      0.44
-    ]
-  },
-  {
-    "ID": 1,
-    "Doc": "Berlin",
-    "EmbeddingEuclidean": [
-      0.05,
-      0.61,
-      0.76,
-      0.74
-    ],
-    "EmbeddingDotProduct": [
-      0.05,
-      0.61,
-      0.76,
-      0.74
-    ],
-    "EmbeddingCosine": [
-      0.05,
-      0.61,
-      0.76,
-      0.74
-    ]
-  }
-]
-    )_";
+  database.LoadDB(dbName, tempDir, 150000, true);
+
+  std::ifstream recordStream("engine/db/testdata/dense_data_1.json");
+  std::string records;
+  recordStream.seekg(0, std::ios::end);
+  records.reserve(recordStream.tellg());
+  recordStream.seekg(0, std::ios::beg);
+  records.assign((std::istreambuf_iterator<char>(recordStream)),
+                 std::istreambuf_iterator<char>());
 
   auto createTableStatus = database.CreateTable(dbName, schema, tableId);
   EXPECT_TRUE(createTableStatus.ok()) << createTableStatus.message();
@@ -926,44 +775,6 @@ TEST(DbServer, RebuildDenseVector) {
   // check rebuild
   auto rebuildStatus = database.Rebuild();
   EXPECT_TRUE(rebuildStatus.ok()) << rebuildStatus.message();
-
-  vectordb::engine::DenseVectorElement queryData[] = {0.35, 0.55, 0.47, 0.94};
-  struct TestCase {
-    std::string searchFieldName;
-    std::vector<std::string> expectedOrder;
-  };
-  std::vector<TestCase> testcases = {
-      {"EmbeddingEuclidean", {"Moscow", "Berlin", "Shanghai", "San Francisco", "London"}},
-      {"EmbeddingDotProduct", {"Moscow", "Berlin", "San Francisco", "London", "Shanghai"}},
-      {"EmbeddingCosine", {"Moscow", "Shanghai", "Berlin", "San Francisco", "London"}}};
-  for (auto &testcase : testcases) {
-    vectordb::Json result;
-    const auto limit = 6;
-    auto queryFields = std::vector<std::string>{"ID", "Doc", testcase.searchFieldName};
-    auto queryStatus = database.Search(dbName, tableName, testcase.searchFieldName, queryFields, queryDimension, queryData, limit, result, "", true);
-    EXPECT_TRUE(queryStatus.ok()) << queryStatus.message();
-    EXPECT_EQ(result.GetSize(), 5) << "duplicate insert should've been ignored";
-    for (int i = 0; i < result.GetSize(); i++) {
-      EXPECT_EQ(result.GetArrayElement(i).GetString("Doc"), testcase.expectedOrder[i])
-          << i << "th city mismatch when querying " << testcase.searchFieldName << std::endl
-          << result.DumpToString();
-    }
-  }
-
-  // test filter
-  for (auto &testcase : testcases) {
-    vectordb::Json result;
-    const auto limit = 6;
-    auto queryFields = std::vector<std::string>{"ID", "Doc", testcase.searchFieldName};
-    auto queryStatus = database.Search(dbName, tableName, testcase.searchFieldName, queryFields, queryDimension, queryData, limit, result, "ID <= 2", true);
-    EXPECT_TRUE(queryStatus.ok()) << queryStatus.message();
-    EXPECT_EQ(result.GetSize(), 2) << "only ID <= 2 entries should be included";
-    for (int i = 0; i < result.GetSize(); i++) {
-      EXPECT_LE(result.GetArrayElement(i).GetInt("ID"), 2) << "returned ID larger than 2"
-                                                           << std::endl
-                                                           << result.DumpToString();
-    }
-  }
 }
 
 TEST(DbServer, RebuildSparseVector) {
