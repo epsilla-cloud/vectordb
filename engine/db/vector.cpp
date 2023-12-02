8828016db81e84c3a46e4f5c043d29cc579fe700
TopKeyboard <lululemonmonitor@gmail.com>
2023-12-02 13:15:16 -0800

    fix unittest

--- engine/db/vector.cpp
+++ engine/db/vector.cpp
@@ -45,6 +45,7 @@ float GetCosineDist(const SparseVector &v1, const SparseVector &v2) {
   }
   return 1 - dot_prod / std::sqrt(v1_prod * v2_prod);
 }
+
 void Normalize(SparseVector &v) {
   float sum = 0;
   for (auto &elem : v) {
