421257b507889ff8466e97a043f2ea8536949a60
richard-epsilla <richard@epsilla.com>
2023-12-13 23:48:57 -0500

    Basic embedding support

--- engine/services/embedding_service.cpp
+++ engine/services/embedding_service.cpp
@@ -5,11 +5,15 @@
 namespace vectordb {
 namespace engine {
 
-EmbeddingService::EmbeddingService(const std::string& base_url) : m_httpClient(base_url) {}
+EmbeddingService::EmbeddingService(const std::string& base_url) {
+  auto requestExecutor = oatpp::curl::RequestExecutor::createShared(base_url);
+  auto objectMapper = oatpp::parser::json::mapping::ObjectMapper::createShared();
+  m_client = MyApiClient::createShared(requestExecutor, objectMapper);
+}
 
 Status EmbeddingService::getSupportedModels(std::vector<EmbeddingModel> &models) {
   try {
-    auto response = m_httpClient.Get("/v1/embeddings");
+    auto response = m_client->getEmbeddings("/v1/embeddings");
     // Parse JSON response and convert to std::vector<EmbeddingModel>
     auto responseBody = response->readBodyToString();
     vectordb::Json json;
@@ -41,86 +45,32 @@ Status EmbeddingService::denseEmbedDocuments(
   size_t end_record,
   size_t dimension
 ) {
-  std::cout << &m_httpClient << std::endl;
-  // try {
-  //   vectordb::Json request;
-  //   request.LoadFromString("{\"model\": \"" + model_name + "\", \"documents\": []}");
-  //   for (auto idx = start_record; idx < end_record; ++idx) {
-  //     request.AddStringToArray("documents", std::get<std::string>(attr_column_container[idx]));
-  //   }
-
-  //   std::cout << request.DumpToString() << std::endl;
-  //   std::cout << &m_httpClient << std::endl;
-  //   std::string req_str = request.DumpToString();
-  //   oatpp::String req_payload = oatpp::String(req_str.c_str(), req_str.size());
-
-
-
-
-
-  //   // CURL *curl;
-  //   // CURLcode res;
-
-  //   // curl_global_init(CURL_GLOBAL_ALL);
-
-  //   // curl = curl_easy_init();
-  //   // if(curl) {
-  //   //     // Set the URL for the POST request
-  //   //     curl_easy_setopt(curl, CURLOPT_URL, "http://runner.epsilla.com:9999/v1/embeddings");
-
-  //   //     // Set the payload (data to send)
-  //   //     std::string payload = "{\"key1\":\"value1\",\"key2\":\"value2\"}";
-
-  //   //     // If posting JSON, you also need to set the content type header
-  //   //     struct curl_slist *headers = NULL;
-  //   //     headers = curl_slist_append(headers, "Content-Type: application/json");
-  //   //     curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
-
-  //   //     // Perform the request, res will get the return code
-  //   //     res = curl_easy_perform(curl);
-
-  //   //     // Check for errors
-  //   //     if(res != CURLE_OK)
-  //   //         fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-
-  //   //     // Clean up headers if used
-  //   //     curl_slist_free_all(headers);
-
-  //   //     // Cleanup
-  //   //     curl_easy_cleanup(curl);
-  //   // }
-
-  //   // curl_global_cleanup();
-
-
-
-
-
-  //   std::cout << "here" << std::endl;
-  //   auto response = m_httpClient.Get("/v1/embeddings");
-  //   // Parse JSON response and convert to std::vector<EmbeddingModel>
-  //   auto responseBody = response->readBodyToString();
-  //   std::cout << "here2" << std::endl;
-    
-
-  //   // auto response = m_httpClient.Post("/v1/embeddings", req_payload);
-  //   // auto responseBody = response->readBodyToString();
-  //   // std::cout << responseBody->c_str() << std::endl;
-  //   // vectordb::Json json;
-  //   // json.LoadFromString(responseBody->c_str());
-  //   // if (json.GetInt("statusCode") == 200) {
-  //   //   auto embeddings = json.GetArray("result");
-  //   //   for (auto idx = start_record; idx < end_record; ++idx) {
-  //   //     auto embedding = embeddings.GetArrayElement(idx);
-  //   //     for (auto i = 0; i < dimension; i++) {
-  //   //       vector_table[idx * dimension + i] = static_cast<float>((float)(embedding.GetArrayElement(i).GetDouble()));
-  //   //     }
-  //   //   }
-  //   //   return Status::OK();
-  //   // }
-  // } catch (const std::exception& e) {
-  //   std::cerr << "Exception in embedDocuments: " << e.what() << std::endl;
-  // }
+  try {
+    auto requestBody = EmbeddingRequestBody::createShared();
+    requestBody->model = model_name;
+    // Constructing documents list from attr_column_container
+    requestBody->documents = oatpp::List<oatpp::String>({});
+    for (size_t idx = start_record; idx < end_record; ++idx) {
+      // Assuming attr_column_container[idx] returns a string or can be converted to string
+      requestBody->documents->push_back(oatpp::String(std::get<std::string>(attr_column_container[idx]).c_str()));
+    }
+    auto response = m_client->denseEmbedDocuments("/v1/embeddings", requestBody);
+    auto responseBody = response->readBodyToString();
+    vectordb::Json json;
+    json.LoadFromString(responseBody->c_str());
+    if (json.GetInt("statusCode") == 200) {
+      auto embeddings = json.GetArray("result");
+      for (auto idx = start_record; idx < end_record; ++idx) {
+        auto embedding = embeddings.GetArrayElement(idx - start_record);
+        for (auto i = 0; i < dimension; i++) {
+          vector_table[idx * dimension + i] = static_cast<float>((float)(embedding.GetArrayElement(i).GetDouble()));
+        }
+      }
+      return Status::OK();
+    }
+  } catch (const std::exception& e) {
+    std::cerr << "Exception in embedDocuments: " << e.what() << std::endl;
+  }
   return Status(INFRA_UNEXPECTED_ERROR, "Failed to embbed the documents.");
 }
 
--- engine/services/embedding_service.hpp
+++ engine/services/embedding_service.hpp
@@ -1,11 +1,18 @@
 #pragma once
 
-#include "utils/http_client.hpp"
 #include "utils/status.hpp"
 #include "utils/json.hpp"
 #include "db/vector.hpp"
 #include <vector>
 #include <string>
+
+#include "oatpp-curl/RequestExecutor.hpp"
+#include "oatpp/web/client/ApiClient.hpp"
+#include "oatpp/core/macro/component.hpp"
+#include "oatpp/core/Types.hpp"
+#include <oatpp/core/macro/codegen.hpp>
+#include "oatpp/codegen/ApiClient_define.hpp"
+#include <oatpp/parser/json/mapping/ObjectMapper.hpp>
 
 namespace vectordb {
 namespace engine {
@@ -15,6 +22,30 @@ constexpr const int EmbeddingRetry = 5;
 constexpr const int EmbeddingBackoffInitialDelaySec = 1;
 constexpr const int EmbeddingBackoffExpBase = 2;
 constexpr const int EmbeddingBackoffJitter = 1;  // 0 means no jitter, 1 means with jitter
+
+#include OATPP_CODEGEN_BEGIN(DTO) ///< Begin DTO codegen section
+
+class EmbeddingRequestBody: public oatpp::DTO {
+
+  DTO_INIT(EmbeddingRequestBody, DTO)
+
+  DTO_FIELD(String, model);
+  DTO_FIELD(List<String>, documents);
+};
+
+#include OATPP_CODEGEN_END(DTO) ///< End DTO codegen section
+
+class MyApiClient : public oatpp::web::client::ApiClient {
+ #include OATPP_CODEGEN_BEGIN(ApiClient) //<- Begin codegen
+
+  API_CLIENT_INIT(MyApiClient)
+
+  API_CALL("GET", "{path}", getEmbeddings, PATH(String, path))
+  API_CALL("POST", "{path}", denseEmbedDocuments, PATH(String, path), BODY_DTO(Object<EmbeddingRequestBody>, body))
+
+
+ #include OATPP_CODEGEN_END(ApiClient) //<- End codegen
+};
 
 struct EmbeddingModel {
   std::string model;
@@ -39,7 +70,7 @@ public:
   );
 
 private:
-  HttpClient m_httpClient;
+  std::shared_ptr<MyApiClient> m_client;
 };
 
 }  // namespace engine
